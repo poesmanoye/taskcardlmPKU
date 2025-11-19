c497d5f2f601d2b4f28a6446d03359b87815d4bc
poesmanoye <poesmanoye@gmail.com>
2025-11-19 09:23:10 +0700

    Update autofill_pdftaskcard.py

--- autofill_pdftaskcard.py
+++ autofill_pdftaskcard.py
@@ -243,8 +243,8 @@ else:
                                     can.drawString(66, 733, work_order)
                                     can.drawString(145, 733, ac_reg)
                                     can.drawString(203, 733, ac_msn)
-                                    can.drawString(273, 733, ac_eff)
-                                    can.drawString(378, 733, operator)                                   
+                                    can.drawString(269, 733, ac_eff)
+                                    can.drawString(375, 733, operator)                                   
 
                             # === WEEKLY BATIK REV 10 ===
                             elif template_name == "TC WEEKLY CHECK A320 BATIK REV 10.pdf":
@@ -364,6 +364,7 @@ else:
 
 # Footer
 st.markdown("<hr><p style='text-align:center;color:#94a3b8;'>Dibuat oleh nomnom_</p>", unsafe_allow_html=True)
+
 
 
 
